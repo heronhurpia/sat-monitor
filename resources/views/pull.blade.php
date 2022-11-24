bc62cc492a6648e20877d0ac85137dd1a2336dd0
heronhurpia <heronhurpia@gmail.com>
2022-11-24 11:44:51 -0300

    nova rota

--- resources/views/pull.blade.php
+++ resources/views/pull.blade.php
@@ -1,6 +1,6 @@
 <?php 
 	echo shell_exec('git pull');
-	echo shell_exec('cd ../..: php artisan migrate') ;
+	echo shell_exec('cd ../.. ; php artisan migrate') ;
 	
 	echo '<p><h5>Executado GIT pull!</h5></p>';
 	echo '<p><a href="lista">Clique aqui para continuar</a></p>';
