95116481dc98c714e25a000eb4fc6777433cf1cc
heronhurpia <heronhurpia@gmail.com>
2022-11-24 11:37:26 -0300

    migrate

--- resources/views/pull.blade.php
+++ resources/views/pull.blade.php
@@ -1,8 +1,8 @@
 <?php 
 	echo shell_exec('git pull');
-	php artisan migrate ;
+	echo shell_exec('php artisan migrate') ;
 	
-	echo '<p><h5>Executado GIT pull!</h5></p>'
-	echo '<p><a href="lista">Clique aqui para continuar</a></p>'
+	echo '<p><h5>Executado GIT pull!</h5></p>';
+	echo '<p><a href="lista">Clique aqui para continuar</a></p>';
 
 ?>
