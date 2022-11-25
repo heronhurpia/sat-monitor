5bd1f237ea2599a122d32ef4721177f8a4641188
heronhurpia <heronhurpia@gmail.com>
2022-11-25 13:47:04 -0300

    Trocadas mensagens de logout e nova senha

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,13 +78,13 @@
 								</a>
 
 								<div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-									
+
 									<a class="dropdown-item" href="{{ route('logout') }}"
 									   onclick="event.preventDefault();
 													 document.getElementById('logout-form').submit();">
-										{{ __('Logout') }}
+										{{ __('Sair') }}
 									</a>
-									<a class="dropdown-item" href="{{ route('change-password') }}">Nova senha</a>
+									<a class="dropdown-item" href="{{ route('change-password') }}">Alterar senha</a>
 
 									<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
 										@csrf
