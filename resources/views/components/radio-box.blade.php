f406f3745c12c05cbb664bd73af3fda725bed579
heronhurpia <heronhurpia@gmail.com>
2022-12-14 10:48:57 -0300

    Melhorado esquema de cor do box de rádio

--- resources/views/components/radio-box.blade.php
+++ resources/views/components/radio-box.blade.php
@@ -1,1 +1,1 @@
-<span style="background-color: rgb(217, 228, 217); padding:2px; color:gray;">&nbspRádio&nbsp</span>
+<span style="background-color: rgb(217, 228, 217); padding:2px; color:gray;)">&nbspRádio&nbsp</span>
